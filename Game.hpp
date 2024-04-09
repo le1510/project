e60d5eaec5e698b9493eacf7ae6794384d8dce3d
le1510 <23020695@vnu.edu.vn>
2024-04-09 20:43:36 +0700

    game

--- Game.hpp
+++ Game.hpp
@@ -1,29 +1,26 @@
 #pragma once
 #include"Config.hpp"
 #include"scenes/BaseScene.hpp"
-//class game
 class Game
 {
 
 private:
 
-    SDL_Window* m_window;// đại diện cho cửa sổ hiển thị trò chơi
-    SDL_Renderer* m_renderer;// trình kết xuất đồ họa để vẽ đồ họa lên cửa sổ
-    BaseScene *m_scene;// con trỏ đến đối tượng lớp basescene đại diện cho cảnh hiện tại đang chạy trong trò chơi
-    bool m_isRunning;// xác định xem trò chơi có đang chạy hay khong
+    SDL_Window* m_window;
+    SDL_Renderer* m_renderer;
+    BaseScene *m_scene;
+    bool m_isRunning;
 public:
     Game();
     ~Game();
     void Run();
     void Quit();
     //sinmgleton pattern
-    static Game* GetInstance();// chỉ có 1 dối tượng duy nhất tạo ra vào truy cập vào chương trình
-    void SetScene(BaseScene*);// đặt cảnh hiện tại của trò chơi
+    static Game* GetInstance();
+    void SetScene(BaseScene*);
 private:
-//ham khoi tao game
     void Initialize();
 
 
-    //ham  huy game
     void Terminate();
 };
--- Resource.hpp
+++ Resource.hpp
@@ -1,21 +1,20 @@
 #pragma once
 
 #include "Config.hpp"
-//chứa các thông tin như font chữ âm thanh hình ảnh phương thức tải tài nguyên từ tệp tin trên đĩa vào bộ nhớ và sử dụng trong trò chơi
 class Resource
 {
 public:
-	static TTF_Font*	FONT_24;//con trỏ đến đối tượng ttf font trong kiẻu dữ liệu thư viện sdl2
+	static TTF_Font*	FONT_24;
     static TTF_Font*	FONT_48;
     static TTF_Font*	FONT_96;
 
-    static Mix_Chunk* SFX_SHOT;//con trỏ trỏ đến thư viện sdl mixer xử lý âm thanh trong ssld, shot là âm thanh súng, background là âm thanh nhạc nền
+    static Mix_Chunk* SFX_SHOT;
     static Mix_Music* SFX_BACKGROUND;
 
 
-	static SDL_Texture* TX_PLAYER;//đại diện óch hình ảnh được tải lên bộ nhớ vẽ đồ họa
+	static SDL_Texture* TX_PLAYER;
 	static SDL_Texture* TX_PLAYER_GUN;
-	static SDL_Texture* TX_PLAYER_BULLET;//dan
+	static SDL_Texture* TX_PLAYER_BULLET;
 
 	static SDL_Texture* TX_MAP_BACKGROUND;
 	static SDL_Texture* TX_MAP_TOWER;
@@ -33,11 +32,11 @@ public:
 	static SDL_Texture* TX_SKELETON_WALK;
 
 public:
-	static SDL_Texture* LoadTextureFromFile(const std::string& path, SDL_Renderer* renderer);//tải 1 hình ảnh từ tệp tin trên đĩa vào bộ nhứo dưới dạng1 đói tượng, aht; đường dãn tới tệp tin, con trỏ đến dl_renderer để tạo và quản lý các đối tượng sdl_texture
+	static SDL_Texture* LoadTextureFromFile(const std::string& path, SDL_Renderer* renderer);
 
-	static TTF_Font* LoadFontFromFile(const std::string& path, int size);// tải font chữ từ 1 tệp tin dưới đường dẫn là patth, kích thước font chữ
+	static TTF_Font* LoadFontFromFile(const std::string& path, int size);
 
     static Mix_Music* LoadMusicFromFile(const std::string&path);
-    static Mix_Chunk* LoadSFXFromFile(const std::string &path);// tải 1 file âm thanh từ 1 đường trên đĩa để tải âm thanh lên
-	static void LoadAllData(SDL_Renderer* renderer);// tải tất cả tai fnguyeen âm thanh hình ảnh font chữ âm thanh nhận đầu vào là con trỏ đến dl_renderer để tạo và quản lý đối tượng dl_texture, tải tất cả lên bộ
+    static Mix_Chunk* LoadSFXFromFile(const std::string &path);
+	static void LoadAllData(SDL_Renderer* renderer);
 };
