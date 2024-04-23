fcae557863e1c40df99f1b9b0b59b0de48156cc3
le1510 <23020695@vnu.edu.vn>
2024-04-24 01:49:04 +0700

    tang giam nut bam

--- scenes/PlayScene.cpp
+++ scenes/PlayScene.cpp
@@ -188,42 +188,61 @@ void PlayScene::Render(SDL_Renderer* renderer)
     {
         bullet->Render(renderer);
     }
-    SDL_Event e;
-    int mouseX = e.motion.x;
-    int mouseY = e.motion.y;
-    if (mouseX >= 0 && mouseX <= 64 && mouseY >= 10 && mouseY <= 74)
+
+
+    SDL_Rect imageRect = { 0, 10, 64, 64  };
+SDL_Point mousePos;
+SDL_GetMouseState(&mousePos.x, &mousePos.y);
+
+if (SDL_PointInRect(&mousePos, &imageRect))
+{
+    SDL_Rect scaledImageRect = { imageRect.x + imageRect.w * 0.25, imageRect.y + imageRect.h * 0.25, imageRect.w * 1.2, imageRect.h * 1.2 };
+    SDL_RenderCopy(renderer, Resource::TX_PAUSE, nullptr, &scaledImageRect);
+}
+else
+{
+    SDL_RenderCopy(renderer, Resource::TX_PAUSE, nullptr, &imageRect);
+}
+
+if(m_isGamePaused)
+{
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+    SDL_Rect rect = { 470, 250, 350, 20 };
+    SDL_RenderFillRect(renderer, &rect);
+
+    SDL_Color white = { 255, 255, 255 };
+    SDL_Surface* surface = TTF_RenderText_Solid(Resource::FONT_24, "Do you want to continue or stop?", white);
+    SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, surface);
+    SDL_Rect textRect = { 500, 250, surface->w, surface->h };
+    SDL_RenderCopy(renderer, texture, nullptr, &textRect);
+    SDL_FreeSurface(surface);
+    SDL_DestroyTexture(texture);
+
+    SDL_Rect continueRect = { 520, 280, 64, 64 };
+    SDL_Rect endRect = { 650, 280, 64, 64 };
+
+    if (SDL_PointInRect(&mousePos, &continueRect))
     {
-        SDL_Rect pauseRect = { 0, 10, 64, 64 };
-        SDL_RenderCopy(renderer, Resource::TX_PAUSE, nullptr, &pauseRect);
+        SDL_Rect scaledContinueRect = { continueRect.x - continueRect.w * 0.25, continueRect.y - continueRect.h * 0.25, continueRect.w * 1.5, continueRect.h * 1.5 };
+        SDL_RenderCopy(renderer, Resource::TX_CONTINUE, nullptr, &scaledContinueRect);
     }
     else
     {
-        SDL_Rect pauseRect = { 0, 10, 64, 64 };
-        SDL_RenderCopy(renderer, Resource::TX_PAUSE, nullptr, &pauseRect);
+        SDL_RenderCopy(renderer, Resource::TX_CONTINUE, nullptr, &continueRect);
     }
 
-    SDL_Rect imageRect = { 0, 10, 64, 64  };
-    SDL_RenderCopy(renderer, Resource::TX_PAUSE, nullptr, &imageRect);
-    if(m_isGamePaused)
+    if (SDL_PointInRect(&mousePos, &endRect))
     {
-        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-        SDL_Rect rect = { 470, 250, 350, 20 };
-        SDL_RenderFillRect(renderer, &rect);
-
-        SDL_Color white = { 255, 255, 255 };
-        SDL_Surface* surface = TTF_RenderText_Solid(Resource::FONT_24, "Do you want to continue or stop?", white);
-        SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, surface);
-        SDL_Rect textRect = { 500, 250, surface->w, surface->h };
-        SDL_RenderCopy(renderer, texture, nullptr, &textRect);
-        SDL_FreeSurface(surface);
-        SDL_DestroyTexture(texture);
-        SDL_Rect imageRect = { 520, 280, 64, 64  };
-        SDL_RenderCopy(renderer, Resource::TX_CONTINUE, nullptr, &imageRect);
-        SDL_Rect image_Rect = { 650, 280, 64, 64  };
-        SDL_RenderCopy(renderer, Resource::TX_END, nullptr, &image_Rect);
-
-
+        SDL_Rect scaledEndRect = { endRect.x - endRect.w * 0.25, endRect.y - endRect.h * 0.25, endRect.w * 1.5, endRect.h * 1.5 };
+        SDL_RenderCopy(renderer, Resource::TX_END, nullptr, &scaledEndRect);
     }
+    else
+    {
+        SDL_RenderCopy(renderer, Resource::TX_END, nullptr, &endRect);
+    }
+}
+
+
 
     this->m_scoreText->RenderText(renderer, "Score: " + std::to_string(this->m_score));
 }
